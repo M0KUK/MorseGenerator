d695f91d9676e3dc0f4dea9fd43e39883a4072e9
m7jqb <jakub.m7jqb@gmail.com>
2023-07-10 21:34:04 +0100

    Guessing game

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 #define ASCII_START 65  //Ascii code of first letter 'A' needed to offset
 #define NUMBER_OF_LETTERS 26
 #define WAIT_TIME 2000 //Time before starting again in miliseconds, e.g. 1000 is 1 second
+#define ASCII_LOWER_A 97  //Ascii code for lowercase 'a'
+#define ASCII_LOWER_Z 122 //Ascii code for lowercase 'z'
+#define ASCII_LOWER_TO_UPPER 32 //Ascii difference between lower- and uppercase
 
 const int buzzerPin = 8; //defines pin that buzzer is connected to
 const int morseTone = 600;  //defines the tone for the morse sound
@@ -12,6 +15,7 @@ const String morseCode[] = {
   ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..",
   "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." 
 };
+int guessLetter = 0; //character provided by user
 
 
 void ditPlay(){
@@ -33,8 +37,6 @@ void play(char whatToPlay){
   }
 }
 
-//TODO add upperCase handling
-
 void playCharacter(char letterToPlay){
   String toPlay = morseCode[letterToPlay- ASCII_START]; //selecting right character
   for (byte i = 0; i < toPlay.length(); i++) //looping through all dits and dahs
@@ -46,16 +48,17 @@ void playCharacter(char letterToPlay){
 
 
 void setup() {
-  // TODO Setting up Serial
+  //Setting up Serial
   Serial.begin(9600);
 
   randomSeed(analogRead(0)); //Pin 0 should be left floating for more random results
-  // Play CQ
-  // playCharacter('C');
-  // delay(dahLength); //end of character
-  // playCharacter('Q');
-
-
+  //Play CQ
+  playCharacter('C');
+  delay(dahLength); //end of character
+  playCharacter('Q');
+  delay(dahLength);
+  
+  delay(WAIT_TIME);
 }
 
 void loop() {
@@ -63,6 +66,23 @@ void loop() {
   char randomLetter = random(NUMBER_OF_LETTERS) + ASCII_START;
   playCharacter(randomLetter);
   delay(dahLength);
+
+  while (guessLetter != randomLetter){
+    if (Serial.available()){
+      guessLetter = Serial.read();
+      //Condition to make all characters uppercase
+      if (guessLetter >= ASCII_LOWER_A && guessLetter <= ASCII_LOWER_Z){
+        guessLetter = guessLetter - ASCII_LOWER_TO_UPPER;
+      }
+      //Presenting error message and replaying a character
+      if (guessLetter != randomLetter){
+        Serial.println("Not quite right. Try again!");
+        playCharacter(randomLetter);
+        delay(dahLength);
+      }
+    }
+  }
+  Serial.println("That's right! Congratulations");
 
   delay(WAIT_TIME);
 }
